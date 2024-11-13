f163efcfb0e56d4805c5e8c50ff09a1d54adddf1
Guilherme Augusto Boquimpani <guilherme2005oficial@gmail.com>
2024-11-13 13:08:21 -0300

    configuração do index, e arrumando o arquivo flask

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, template_folder='templates')
-app.config['SQLALCHMY_DATABASE_URI'] = 'sqlite:///estudantes.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///estudantes.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 
 db = SQLAlchemy(app)
 
 class Estudante(db.Model):
-    id = db.column('id', db.integer, primary_key=True, autoincrement=True)
-    nome = db.column(db.string(150))
-    idade = db.column(db.integer) 
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nome = db.Column(db.String(150))
+    idade = db.Column(db.Integer) 
 
     def __init__(self, nome, idade):
         self.nome = nome
@@ -23,8 +24,10 @@ class Estudante(db.Model):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    estudantes = Estudante.query.all()
+    return render_template('index.html', estudantes=estudantes)
 
 if __name__ == '__main__':
-    db.create_all()
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
